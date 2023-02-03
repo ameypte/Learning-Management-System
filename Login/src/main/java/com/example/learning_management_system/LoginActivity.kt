b042ef9c8a160c6c897b3089d576842cd06bbaa4
Anant Deshmukh <anant7564@gmail.com>
2023-02-03 22:14:01 +0530

    Validation for user input & output and connecting kotlin files to each other

--- Login/src/main/java/com/example/learning_management_system/LoginActivity.kt
+++ Login/src/main/java/com/example/learning_management_system/LoginActivity.kt
@@ -1,11 +1,41 @@
 package com.example.learning_management_system
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        val btnlog = findViewById<Button>(R.id.btnLogin)
+        val btncr = findViewById<Button>(R.id.btnCreate)
+
+        btnlog.setOnClickListener{
+            val Idcode = findViewById<EditText>(R.id.etIdCode)
+            val icode = Idcode.text.toString()
+
+            val Pass = findViewById<EditText>(R.id.etPassword)
+            val pass = Pass.text.toString()
+
+            if (icode.isBlank() || pass.isBlank()){
+                Toast.makeText(this, "Please insert all the datail", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Login successfully", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btncr.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Login/src/main/java/com/example/learning_management_system/RegisterActivity.kt
+++ Login/src/main/java/com/example/learning_management_system/RegisterActivity.kt
@@ -1,12 +1,60 @@
 package com.example.learning_management_system
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
+import java.util.jar.Attributes.Name
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         title = "Register"
+
+        val btreg = findViewById<Button>(R.id.btnRegister)
+        val btnalready = findViewById<Button>(R.id.btnAlready)
+
+        btreg.setOnClickListener {
+
+            val Name = findViewById<EditText>(R.id.etName)
+            val name = Name.text.toString()
+
+            val Icode = findViewById<EditText>(R.id.etId)
+            val uid = Icode.text.toString()
+
+//            val regen = findViewById<RadioGroup>(R.id.rgGender)
+//            val gen = regen.Button.toString()
+
+            val Mail = findViewById<EditText>(R.id.etEmail)
+            val email = Mail.text.toString()
+
+            val Phone = findViewById<EditText>(R.id.etPhone)
+            val phone = Phone.text.toString()
+
+            val Pass = findViewById<EditText>(R.id.etPass)
+            val pass = Pass.text.toString()
+
+            val Conpass = findViewById<EditText>(R.id.etConPass)
+            val conpass = Conpass.text.toString()
+
+            if(name.isBlank() || uid.isBlank() || email.isBlank() || phone.isBlank() || pass.isBlank()){
+                Toast.makeText(this, "Please insert all the datail", Toast.LENGTH_SHORT).show()
+            }
+            else if (pass != conpass){
+                Toast.makeText(this, "Password and Confirm Password didn't match", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Register successfully", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnalready.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
