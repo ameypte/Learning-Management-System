317fcfeb3490204f39d29ba2847fe527b56b9eec
mailt <ameypte@gmail.com>
2023-02-04 10:35:22 +0530

    enable the view binding in the module and created Student.kt data class

--- Login/src/main/java/com/example/learning_management_system/RegisterActivity.kt
+++ Login/src/main/java/com/example/learning_management_system/RegisterActivity.kt
@@ -4,21 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.example.learning_management_system.databinding.ActivityRegisterBinding
+import com.google.firebase.database.DatabaseReference
 
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var radioGroup: RadioGroup
     private lateinit var selectedRadioButton: RadioButton
 
+    // database code
+    // creating the binding for activity_register.xml
+    private lateinit var registerBinding: ActivityRegisterBinding
+    private lateinit var databse: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        // database code
+        registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(registerBinding.root)
+
         title = "Register"
 
-        val btreg = findViewById<Button>(R.id.btnRegister)
+//        val btreg = findViewById<Button>(R.id.btnRegister)
         val btnalready = findViewById<Button>(R.id.btnAlready)
 
-        btreg.setOnClickListener {
+        registerBinding.btnRegister.setOnClickListener {
 
             val Name = findViewById<EditText>(R.id.etName)
             val name = Name.text.toString()
@@ -43,17 +54,18 @@ class RegisterActivity : AppCompatActivity() {
             val Conpass = findViewById<EditText>(R.id.etConPass)
             val conpass = Conpass.text.toString()
 
-            if(name.isBlank() || uid.isBlank() || email.isBlank() || phone.isBlank() || pass.isBlank()){
+            if (name.isBlank() || uid.isBlank() || email.isBlank() || phone.isBlank() || pass.isBlank()) {
                 Toast.makeText(this, "Please insert all the detail", Toast.LENGTH_SHORT).show()
-            }
-            else if (pass != conpass){
-                Toast.makeText(this, "Password and Confirm Password didn't match", Toast.LENGTH_SHORT).show()
-            }
-            else{
+            } else if (pass != conpass) {
+                Toast.makeText(
+                    this, "Password and Confirm Password didn't match", Toast.LENGTH_SHORT
+                ).show()
+            } else {
                 if (selectedRadioButtonId != -1) {
                     selectedRadioButton = findViewById(selectedRadioButtonId)
                     val string: String = selectedRadioButton.text.toString()
-                    Toast.makeText(this, "Register successfully, $string", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Register successfully, $string", Toast.LENGTH_SHORT)
+                        .show()
                 } else {
                     Toast.makeText(this, "Please Select The gender!", Toast.LENGTH_SHORT).show()
                 }
---
+++ Login/src/main/java/com/example/learning_management_system/Student.kt
@@ -0,0 +1,12 @@
+package com.example.learning_management_system
+
+data class Student(
+    val name: String? = null,
+    val idCode: String? = null,
+    val gender: String? = null,
+    val email: String? = null,
+    val phone: String? = null,
+    val password: String? = null
+) {
+
+}
