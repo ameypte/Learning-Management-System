bf4f96dbbf70df7854552eb45d269b40818b9950
Anuja <anujatongse@gmail.com>
2023-02-06 14:53:50 +0530

    added reset password page

--- Login/src/main/java/com/example/learning_management_system/ForgotPasswordActivity.kt
+++ Login/src/main/java/com/example/learning_management_system/ForgotPasswordActivity.kt
@@ -1,11 +1,22 @@
 package com.example.learning_management_system
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.learning_management_system.databinding.ActivityForgotPasswordBinding
+import com.example.learning_management_system.databinding.ActivityLoginBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var forgotBinding: ActivityForgotPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        forgotBinding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(forgotBinding.root)
+
+        forgotBinding.btnContinue.setOnClickListener {
+            val intent = Intent(this,ResetPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
