a2f48c994fa9d8e3bbf2abd755281919fb0262d8
Anant Deshmukh <anant7564@gmail.com>
2023-02-07 19:41:08 +0530

    Verify id & email for ForgotPasswordActivity.kt And Send user to ResetPasswordActivity.kt

--- Dashboard/src/main/java/com/example/dashboard/Dashboard.kt
+++ Dashboard/src/main/java/com/example/dashboard/Dashboard.kt
@@ -1,6 +1,7 @@
 package com.example.dashboard
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
