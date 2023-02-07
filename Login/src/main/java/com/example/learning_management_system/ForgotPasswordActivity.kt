56151e020620174eedcc5088526509c5642fdf99
Anant Deshmukh <anant7564@gmail.com>
2023-02-07 17:43:10 +0530

    Validation for Forgot Passward, Reset Password & Updating password in database

--- Login/src/main/java/com/example/learning_management_system/ForgotPasswordActivity.kt
+++ Login/src/main/java/com/example/learning_management_system/ForgotPasswordActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.learning_management_system.databinding.ActivityForgotPasswordBinding
-import com.example.learning_management_system.databinding.ActivityLoginBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
     private lateinit var forgotBinding: ActivityForgotPasswordBinding
@@ -15,8 +14,19 @@ class ForgotPasswordActivity : AppCompatActivity() {
         setContentView(forgotBinding.root)
 
         forgotBinding.btnContinue.setOnClickListener {
-            val intent = Intent(this,ResetPasswordActivity::class.java)
-            startActivity(intent)
+
+            val id = forgotBinding.etIdCode.text.toString()
+            val email = forgotBinding.etEmail.text.toString()
+
+
+            if(id.isBlank() || email.isBlank()){
+                Toast.makeText(this, "Please insert all the detail", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, ResetPasswordActivity::class.java)
+                intent.putExtra("id", id)
+                startActivity(intent)
+            }
         }
     }
 }
--- Login/src/main/java/com/example/learning_management_system/ResetPasswordActivity.kt
+++ Login/src/main/java/com/example/learning_management_system/ResetPasswordActivity.kt
@@ -2,10 +2,44 @@ package com.example.learning_management_system
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.learning_management_system.databinding.ActivityResetPasswordBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ResetPasswordActivity : AppCompatActivity() {
+    private lateinit var resetBinding: ActivityResetPasswordBinding
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
+
+        resetBinding = ActivityResetPasswordBinding.inflate(layoutInflater)
+        setContentView(resetBinding.root)
+
+        val itemList = intent.getStringExtra("id")
+
+        // getting the reference of realtime database
+        database = FirebaseDatabase.getInstance().getReference("Student")
+
+
+        resetBinding.btnResetPass.setOnClickListener {
+            val newpass = resetBinding.etNewPass.text.toString()
+            val conpass = resetBinding.etConPass.text.toString()
+
+            if (newpass.isBlank() || conpass.isBlank()) {
+                Toast.makeText(this, "Please insert all the datail", Toast.LENGTH_SHORT).show()
+            } else
+                if (newpass != conpass) {
+                    Toast.makeText(this, "Password and Confirm Password didn't match", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (intent != null) {
+                        Toast.makeText(this, intent.getStringExtra("id"), Toast.LENGTH_SHORT).show()
+                        database.child("Student").child(itemList.toString()).child("password").setValue(conpass)
+                    }
+
+                }
+        }
+
     }
 }
