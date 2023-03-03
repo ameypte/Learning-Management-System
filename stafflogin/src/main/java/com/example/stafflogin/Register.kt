986220cb67f5fd5c2145bf785317234070b26cd3
Anant Deshmukh <anant7564@gmail.com>
2023-03-03 17:38:43 +0530

    Added Register page layout

--- stafflogin/src/main/java/com/example/stafflogin/Register.kt
+++ stafflogin/src/main/java/com/example/stafflogin/Register.kt
@@ -2,6 +2,9 @@ package com.example.stafflogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
