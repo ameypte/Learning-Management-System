c35723f83f5933aba682bb4804f53f35e0b4601c
Amey Pathe <ameypte@gmail.com>
2023-03-03 17:47:16 +0530

    added scroll and spinner values

--- stafflogin/src/main/java/com/example/stafflogin/Register.kt
+++ stafflogin/src/main/java/com/example/stafflogin/Register.kt
@@ -1,7 +1,11 @@
 package com.example.stafflogin
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.stafflogin.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
@@ -11,6 +15,27 @@ class Register : AppCompatActivity() {
         registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(registerBinding.root)
 
+        val spinner = registerBinding.spinner
+        val items = arrayOf(
+            "Select Department",
+            "Information Technology",
+            "Computer Engineering",
+            "Civil Engineering",
+            "Mechanical Engineering",
+            "ExTc Engineering",
+            "Electrical Engineering",
+            "Chemical Engineering",
+            "Plastic & Polymer Engineering"
+        )
+        val adapter =
+            ArrayAdapter<String>(this, R.layout.simple_spinner_dropdown_item, items)
+        spinner.adapter = adapter
 
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(arg0: AdapterView<*>?, arg1: View?, arg2: Int, arg3: Long) {
+                val items1 = spinner.selectedItem.toString()
+            }
+            override fun onNothingSelected(arg0: AdapterView<*>?) {}
+        }
     }
 }
