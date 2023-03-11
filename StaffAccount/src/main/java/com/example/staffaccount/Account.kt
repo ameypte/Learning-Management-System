08c2c29712ef588deb27bf6de141f15311b0fd98
Anant Deshmukh <anant7564@gmail.com>
2023-03-12 00:45:00 +0530

    designed the account page

--- StaffAccount/src/main/java/com/example/staffaccount/Account.kt
+++ StaffAccount/src/main/java/com/example/staffaccount/Account.kt
@@ -1,25 +1,28 @@
 package com.example.staffaccount
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.staffaccount.databinding.FragmentAccountBinding
+import com.example.stafflogin.Login
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Account.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Account : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var accountBinding:FragmentAccountBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,21 +35,24 @@ class Account : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+    ): View{
+        accountBinding=FragmentAccountBinding.inflate(inflater,container,false)
+        sharedPreferences = requireContext().getSharedPreferences(
+            getString(com.example.staffdashboard.R.string.login_preference_file_name),
+            Context.MODE_PRIVATE
+        )
+
+        accountBinding.btnLogout.setOnClickListener {
+            sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
+            val intent = Intent(requireContext(),Login::class.java)
+            startActivity(intent)
+        }
+
+        return accountBinding.root
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Account.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Account().apply {
