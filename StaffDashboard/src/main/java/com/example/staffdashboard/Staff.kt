19c742944d8b05cdc036b4ae17e14246400670d4
Anant Deshmukh <anant7564@gmail.com>
2023-03-24 23:39:24 +0530

    Updated database for edit staff_account_info and display it on account fragment

---
+++ StaffDashboard/src/main/java/com/example/staffdashboard/Staff.kt
@@ -0,0 +1,7 @@
+package com.example.staffdashboard
+
+data class Staff(val name: String? = null,
+                 val email: String? = null,
+                 val branch: String? = null,
+                 val phone: String? = null
+)
--- StaffDashboard/src/main/java/com/example/staffdashboard/StaffEdit.kt
+++ StaffDashboard/src/main/java/com/example/staffdashboard/StaffEdit.kt
@@ -1,13 +1,19 @@
 package com.example.staffdashboard
 
+import android.R
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.staffdashboard.databinding.FragmentCoursesBinding
+import androidx.fragment.app.FragmentActivity
 import com.example.staffdashboard.databinding.FragmentStaffEditBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +25,10 @@ class StaffEdit : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var staffEditBinding: FragmentStaffEditBinding
+    private lateinit var database: DatabaseReference
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var mob: String
+    private lateinit var dept: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +43,42 @@ class StaffEdit : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         staffEditBinding = FragmentStaffEditBinding.inflate(inflater, container, false)
+        sharedPreferences = requireContext().getSharedPreferences(
+            getString(com.example.staffdashboard.R.string.login_preference_file_name),
+            Context.MODE_PRIVATE
+        )
+
 
         staffEditBinding.btnEdit.setOnClickListener {
-            Toast.makeText(requireContext(),"Edit Successfully",Toast.LENGTH_SHORT).show()
+//          val edmob = staffEditBinding.etMobValue.text.toString()
+            val edname = staffEditBinding.etNameValue.text.toString()
+            val edmail = staffEditBinding.etMailValue.text.toString()
+            savePreferences(edname, edmail)
+
+            database = FirebaseDatabase.getInstance().getReference("Departments")
+            val Staff = Staff(edname, edmail)
+            if (edname.isBlank() || edmail.isBlank()){
+                Toast.makeText(requireContext(), "Please insert all the detail", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                mob = sharedPreferences.getString("loggedStaffPhone","").toString()
+                dept = sharedPreferences.getString("loggedStaffDepartment","").toString()
+//                database.child(dept).child("Staff").child(mob).child("phone").setValue(edmob)
+                database.child(dept).child("Staff").child(mob).child("name").setValue(edname)
+                database.child(dept).child("Staff").child(mob).child("email").setValue(edmail)
+                    .addOnSuccessListener {
+                        requireActivity().supportFragmentManager.popBackStack()
+                    }.addOnCompleteListener {
+//                registerBinding.prBarRegister.visibility = View.GONE
+                }
+            }
         }
         return staffEditBinding.root
+    }
+
+    private fun savePreferences(edname: Any, edmail: Any) {
+        sharedPreferences.edit().putString("loggedStaffName", edname.toString()).apply()
+        sharedPreferences.edit().putString("loggedStaffEmail", edmail.toString()).apply()
     }
 
     companion object {
