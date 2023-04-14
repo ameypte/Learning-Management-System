110759b79e37d188c9bd7402f6a329ef5fe16c54
Anant Deshmukh <anant7564@gmail.com>
2023-04-14 18:47:44 +0530

    updated version of upload time table page

--- StaffDashboard/src/main/java/com/example/staffdashboard/UploadTimeTable.kt
+++ StaffDashboard/src/main/java/com/example/staffdashboard/UploadTimeTable.kt
@@ -1,18 +1,16 @@
 package com.example.staffdashboard
 
 import android.app.TimePickerDialog
+import android.content.SharedPreferences
 import android.icu.util.Calendar
-import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.TimePicker
-import android.widget.Toast
+import android.widget.*
 import com.example.staffdashboard.databinding.FragmentUploadTimeTableBinding
+import com.google.firebase.database.DatabaseReference
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -21,7 +19,14 @@ private const val ARG_PARAM2 = "param2"
 class UploadTimeTable : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+    private var batch: String? = null
     private lateinit var uploadTimeTableBinding: FragmentUploadTimeTableBinding
+    private lateinit var database: DatabaseReference
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var selectedType: RadioButton
+    private lateinit var selectedBatch: RadioButton
+    private lateinit var startTime: String
+    private lateinit var endTime: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +37,7 @@ class UploadTimeTable : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         uploadTimeTableBinding = FragmentUploadTimeTableBinding.inflate(inflater, container, false)
@@ -83,23 +88,61 @@ class UploadTimeTable : Fragment() {
         }
 
         uploadTimeTableBinding.typePra.setOnClickListener {
+            uploadTimeTableBinding.tvType2.setText("Practical")
             uploadTimeTableBinding.rgBatch.visibility = View.VISIBLE
+            uploadTimeTableBinding.tvBatchName.visibility = View.VISIBLE
         }
         uploadTimeTableBinding.typeLec.setOnClickListener {
+            uploadTimeTableBinding.tvType2.setText("Lecture")
             uploadTimeTableBinding.rgBatch.visibility = View.GONE
+            uploadTimeTableBinding.tvBatchName.visibility = View.GONE
+        }
+
+        uploadTimeTableBinding.batch1.setOnClickListener {
+            uploadTimeTableBinding.tvBatchName.setText("IF1")
+        }
+        uploadTimeTableBinding.batch2.setOnClickListener {
+            uploadTimeTableBinding.tvBatchName.setText("IF2")
+        }
+        uploadTimeTableBinding.batch2.setOnClickListener {
+            uploadTimeTableBinding.tvBatchName.setText("IF3")
         }
 
         uploadTimeTableBinding.btnStart.setOnClickListener {
-            timePicker()
+            timePicker{selectedTime ->
+                startTime = selectedTime
+            }
         }
         uploadTimeTableBinding.btnEnd.setOnClickListener {
-            val endTime = timePicker()
-            Toast.makeText(requireContext(), endTime,Toast.LENGTH_SHORT).show()
+            timePicker { selectedTime ->
+                endTime = selectedTime
+            }
+        }
+
+        uploadTimeTableBinding.btnAdd.setOnClickListener {
+            val selectedTypeId: Int = uploadTimeTableBinding.rgType.checkedRadioButtonId
+            selectedType = requireView().findViewById(selectedTypeId)
+
+            val day = uploadTimeTableBinding.spDay.selectedItem.toString()
+            val year = uploadTimeTableBinding.spYear.selectedItem.toString()
+            val type: String = selectedType.text.toString()
+
+            if (type == "Practical") {
+                val selectedBatchId: Int = uploadTimeTableBinding.rgBatch.checkedRadioButtonId
+                selectedBatch = requireView().findViewById(selectedBatchId)
+                batch = selectedBatch.text.toString()
+            } else{
+                batch =""
+            }
+
+//            val name = sharedPreferences.getString("loggedStaffDepartment", "Name").toString()
+
+            Toast.makeText(requireContext(),"$type $day $year\n $batch $startTime $endTime",Toast.LENGTH_SHORT).show()
         }
         return uploadTimeTableBinding.root
     }
 
-    private fun timePicker():String {
+    private fun timePicker(callback: (String) -> Unit) {
         val currentTime = Calendar.getInstance()
         var hour = currentTime.get(Calendar.HOUR_OF_DAY)
         val minute = currentTime.get(Calendar.MINUTE)
@@ -108,12 +151,12 @@ class UploadTimeTable : Fragment() {
             requireContext(),
             {_,hourOfDay, minute ->
                 hour = hourOfDay
-//                Toast.makeText(requireContext(), "Selected Time: $hourOfDay:$minute", Toast.LENGTH_SHORT).show()
+                val selectedTime = "$hourOfDay:$minute"
+                callback(selectedTime)
             },
             hour, minute, false
         )
         timePicker.show()
-        return "$hour:$minute"
     }
 
     companion object {
